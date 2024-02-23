eb648838a8989a04d7f67630a919bf8e39bcf909
brobo1 <armanahz95@gmail.com>
2024-02-23 12:42:59 +0100

    Update Counter components to use unique keys
    Components in Counter and CounterHistory are now using unique keys instead of index values. Additional changes include storing counter state as an object that includes both the value and an ID for tracking individual changes. Also, an unused useEffect hook is commented out.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter            from './components/Counter/Counter.jsx';
+import Header             from './components/Header.jsx';
+import {log}              from './log.js';
 import {ConfigureCounter} from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
   log('<App /> rendered');
-
+  
   const [chosenCount, setChosenCount] = useState(0);
-
+  
   function handleSetCount(newCount) {
-    setChosenCount(newCount);
+	setChosenCount(newCount);
   }
-
+  
   return (
-    <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
-        <Counter initialCount={0} />
-      </main>
-    </>
+	<>
+	  <Header/>
+	  <main>
+		<ConfigureCounter onSet={handleSetCount}/>
+		<Counter key={chosenCount} initialCount={chosenCount}/>
+		<Counter initialCount={0}/>
+	  </main>
+	</>
   );
 }
 
